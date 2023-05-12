e45cf94c29fd3cbb223ef83b3420e14bc0b1154e
aderama2711 <ade.ramadha@gmail.com>
2023-05-12 15:10:31 +0700

    Fix server

--- producer.go
+++ producer.go
@@ -3,31 +3,77 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"math/rand"
 
 	"github.com/usnistgov/ndn-dpdk/ndn"
 	"github.com/usnistgov/ndn-dpdk/ndn/endpoint"
+	"github.com/usnistgov/ndn-dpdk/ndn/l3"
+	"github.com/usnistgov/ndn-dpdk/ndn/memiftransport"
+	"github.com/usnistgov/ndn-dpdk/ndn/mgmt"
+	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/gqlmgmt"
+	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/nfdmgmt"
 )
 
-func main() {
-	for {
-		payload := make([]byte, 1024)
-		rand.New(rand.NewSource(rand.Int63())).Read(payload)
+var (
+	gqlserver string
+	mtuFlag   int
+	useNfd    bool
+	enableLog bool
 
-		p, e := endpoint.Produce(context.Background(), endpoint.ProducerOptions{
-			Prefix:      ndn.ParseName("/ndn/coba"),
-			NoAdvertise: false,
-			Handler: func(ctx context.Context, interest ndn.Interest) (ndn.Data, error) {
-				fmt.Print(interest)
-				return ndn.MakeData(interest, payload), nil
-			},
-		})
+	client mgmt.Client
+	face   mgmt.Face
+	fwFace l3.FwFace
+)
 
-		if e != nil {
-			fmt.Print(e)
-		}
+func openUplink() (e error) {
+	client, e = nfdmgmt.New()
 
-		defer p.Close()
+	switch client := client.(type) {
+	case *gqlmgmt.Client:
+		var loc memiftransport.Locator
+		loc.Dataroom = mtuFlag
+		face, e = client.OpenMemif(loc)
+	default:
+		face, e = client.OpenFace()
 	}
+	if e != nil {
+		return e
+	}
+	l3face := face.Face()
+
+	fw := l3.GetDefaultForwarder()
+	if fwFace, e = fw.AddFace(l3face); e != nil {
+		return e
+	}
+	fwFace.AddRoute(ndn.Name{})
+	fw.AddReadvertiseDestination(face)
+
+	log.Printf("uplink opened, state is %s", l3face.State())
+	l3face.OnStateChange(func(st l3.TransportState) {
+		log.Printf("uplink state changes to %s", l3face.State())
+	})
+	return nil
+}
+
+func main() {
+	openUplink()
+	payload := make([]byte, 1024)
+	rand.New(rand.NewSource(rand.Int63())).Read(payload)
+
+	p, e := endpoint.Produce(context.Background(), endpoint.ProducerOptions{
+		Prefix:      ndn.ParseName("/ndn/coba"),
+		NoAdvertise: false,
+		Handler: func(ctx context.Context, interest ndn.Interest) (ndn.Data, error) {
+			fmt.Print(interest)
+			return ndn.MakeData(interest, payload), nil
+		},
+	})
+
+	if e != nil {
+		fmt.Print(e)
+	}
+
+	defer p.Close()
 
 }
