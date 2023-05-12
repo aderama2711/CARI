21a94ece6d9d600db874d199943bb5dd37bf5832
aderama2711 <ade.ramadha@gmail.com>
2023-05-12 10:51:50 +0700

    Try to fix #8

--- producer.go
+++ producer.go
@@ -14,7 +14,7 @@ func main() {
 		payload := make([]byte, 1024)
 		rand.New(rand.NewSource(rand.Int63())).Read(payload)
 
-		p, e := endpoint.Produce(context.Background(), endpoint.ProducerOptions{
+		_, e := endpoint.Produce(context.Background(), endpoint.ProducerOptions{
 			Prefix:      ndn.ParseName("/ndn/coba"),
 			NoAdvertise: false,
 			Handler: func(ctx context.Context, interest ndn.Interest) (ndn.Data, error) {
@@ -26,7 +26,6 @@ func main() {
 		if e != nil {
 			fmt.Print(e)
 		}
-		defer p.Close()
 	}
 
 }
