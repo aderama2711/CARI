7605cf558b5b6755ca12ff9cea5daf266281f2af
aderama2711 <ade.ramadha@gmail.com>
2023-05-12 15:14:50 +0700

    Fix server #1

--- producer.go
+++ producer.go
@@ -58,22 +58,24 @@ func openUplink() (e error) {
 
 func main() {
 	openUplink()
-	payload := make([]byte, 1024)
-	rand.New(rand.NewSource(rand.Int63())).Read(payload)
+	for {
+		payload := make([]byte, 1024)
+		rand.New(rand.NewSource(rand.Int63())).Read(payload)
 
-	p, e := endpoint.Produce(context.Background(), endpoint.ProducerOptions{
-		Prefix:      ndn.ParseName("/ndn/coba"),
-		NoAdvertise: false,
-		Handler: func(ctx context.Context, interest ndn.Interest) (ndn.Data, error) {
-			fmt.Print(interest)
-			return ndn.MakeData(interest, payload), nil
-		},
-	})
+		p, e := endpoint.Produce(context.Background(), endpoint.ProducerOptions{
+			Prefix:      ndn.ParseName("/ndn/coba"),
+			NoAdvertise: false,
+			Handler: func(ctx context.Context, interest ndn.Interest) (ndn.Data, error) {
+				fmt.Print(interest)
+				return ndn.MakeData(interest, payload), nil
+			},
+		})
 
-	if e != nil {
-		fmt.Print(e)
+		if e != nil {
+			fmt.Print(e)
+		}
+
+		defer p.Close()
 	}
-
-	defer p.Close()
 
 }
