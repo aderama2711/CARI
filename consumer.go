6f13a463b54fb0fcb1451f079b9e900601077f5a
aderama2711 <ade.ramadha@gmail.com>
2023-05-12 11:01:50 +0700

    Try to fix #9

--- consumer.go
+++ consumer.go
@@ -11,22 +11,22 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	for {
+		seqNum := rand.Uint64()
 
-	seqNum := rand.Uint64()
+		var nData, nErrors atomic.Int64
 
-	var nData, nErrors atomic.Int64
+		interest := ndn.MakeInterest(fmt.Sprintf("/ndn/coba/%016X", seqNum), ndn.MustBeFreshFlag)
 
-	interest := ndn.MakeInterest(fmt.Sprintf("/ndn/coba/%016X", seqNum), ndn.MustBeFreshFlag)
+		_, e := endpoint.Consume(context.Background(), interest, endpoint.ConsumerOptions{})
 
-	_, e := endpoint.Consume(ctx, interest, endpoint.ConsumerOptions{})
-
-	if e == nil {
-		nDataL, nErrorsL := nData.Add(1), nErrors.Load()
-		fmt.Printf("%6.2f%% D %016X %6dus", 100*float64(nDataL)/float64(nDataL+nErrorsL), seqNum)
-	} else {
-		nDataL, nErrorsL := nData.Load(), nErrors.Add(1)
-		fmt.Printf("%6.2f%% E %016X %v", 100*float64(nDataL)/float64(nDataL+nErrorsL), seqNum, e)
+		if e == nil {
+			nDataL, nErrorsL := nData.Add(1), nErrors.Load()
+			fmt.Printf("%6.2f%% D %016X %6dus", 100*float64(nDataL)/float64(nDataL+nErrorsL), seqNum)
+		} else {
+			nDataL, nErrorsL := nData.Load(), nErrors.Add(1)
+			fmt.Printf("%6.2f%% E %016X %v", 100*float64(nDataL)/float64(nDataL+nErrorsL), seqNum, e)
+		}
 	}
+
 }
