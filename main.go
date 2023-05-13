2d75dca2f5bbc344f5950e70e6711ab46722a961
aderama2711 <ade.ramadha@gmail.com>
2023-05-13 13:51:39 +0700

    get face

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"context"
+	"log"
 	"time"
+
+	"github.com/usnistgov/ndn-dpdk/ndn"
+	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/nfdmgmt"
 )
 
 func main() {
-	//Serve /hello interest
-	go serve_hello("R1")
 
-	//hello protocol every 5 second
-	go consum_hello(5)
+	get_face()
+
+	// //Serve /hello interest
+	// go serve_hello("R1")
+
+	// //hello protocol every 5 second
+	// go consum_hello(5)
 }
 
 func serve_hello(router string) {
@@ -21,5 +29,23 @@ func consum_hello(delay int) {
 	for {
 		consumer("/hello")
 		time.Sleep(interval)
+	}
+}
+
+func get_face() {
+	client, e := nfdmgmt.New()
+	if e != nil {
+		return e
+	}
+
+	client.Prefix = ndn.ParseName("/localhost/nfd")
+
+	client.Inv
+
+	cr, e := client.Client.Invoke(context.Background(), ndn.ParseName("/face/list"))
+	if e != nil {
+		log.Printf("%v", e)
+	} else {
+		log.Printf("%d", cr.StatusCode)
 	}
 }
