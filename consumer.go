cf3da94442613fcf935fc5858ed68eca1a8c75ba
aderama2711 <ade.ramadha@gmail.com>
2023-05-13 10:08:29 +0700

    create library file

--- consumer.go
+++ consumer.go
@@ -3,58 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"sync/atomic"
 
 	"github.com/usnistgov/ndn-dpdk/ndn"
 	"github.com/usnistgov/ndn-dpdk/ndn/endpoint"
-	"github.com/usnistgov/ndn-dpdk/ndn/l3"
-	"github.com/usnistgov/ndn-dpdk/ndn/memiftransport"
-	"github.com/usnistgov/ndn-dpdk/ndn/mgmt"
-	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/gqlmgmt"
-	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/nfdmgmt"
 )
-
-var (
-	gqlserver string
-	mtuFlag   int
-	useNfd    bool
-	enableLog bool
-
-	client mgmt.Client
-	face   mgmt.Face
-	fwFace l3.FwFace
-)
-
-func openUplink() (e error) {
-	client, e = nfdmgmt.New()
-
-	switch client := client.(type) {
-	case *gqlmgmt.Client:
-		var loc memiftransport.Locator
-		loc.Dataroom = mtuFlag
-		face, e = client.OpenMemif(loc)
-	default:
-		face, e = client.OpenFace()
-	}
-	if e != nil {
-		return e
-	}
-	l3face := face.Face()
-
-	fw := l3.GetDefaultForwarder()
-	if fwFace, e = fw.AddFace(l3face); e != nil {
-		return e
-	}
-	fwFace.AddRoute(ndn.Name{})
-	fw.AddReadvertiseDestination(face)
-
-	log.Printf("uplink opened, state is %s", l3face.State())
-	l3face.OnStateChange(func(st l3.TransportState) {
-		log.Printf("uplink state changes to %s", l3face.State())
-	})
-	return nil
-}
 
 func main() {
 	openUplink()
@@ -64,11 +17,12 @@ func main() {
 
 		name := ndn.ParseName("/ndn/coba")
 
-		_, e := endpoint.Consume(context.Background(), ndn.MakeInterest(name),
+		data, e := endpoint.Consume(context.Background(), ndn.MakeInterest(name),
 			endpoint.ConsumerOptions{})
 
 		if e == nil {
 			nDataL, nErrorsL := nData.Add(1), nErrors.Load()
+			fmt.Println(data.Content)
 			fmt.Printf("%6.2f%% D %6dus\n", 100*float64(nDataL)/float64(nDataL+nErrorsL))
 		} else {
 			nDataL, nErrorsL := nData.Load(), nErrors.Add(1)
