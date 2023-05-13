138e68067b43179f5db5b8aa836eb50ee306f0ed
aderama2711 <ade.ramadha@gmail.com>
2023-05-13 11:34:14 +0700

    hello protocol

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import (
+	"time"
+)
+
 func main() {
-	producer("/ndn/coba", "R1")
-	//consumer("/ndn/coba")
+	//Serve /hello interest
+	go serve_hello("R1")
+
+	//hello protocol every 5 second
+	go consum_hello(5)
+}
+
+func serve_hello(router string) {
+	producer("/hello", router)
+}
+
+func consum_hello(delay int) {
+	interval := 5 * time.Second
+	for {
+		consumer("/hello")
+		time.Sleep(interval)
+	}
 }
