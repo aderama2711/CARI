2ab5cbf9194ec99d02b578badfb14ff8d09f3c01
aderama2711 <ade.ramadha@gmail.com>
2023-06-16 14:07:30 +0700

    only support UDP connection

--- parse_facelist.go
+++ parse_facelist.go
@@ -142,7 +142,7 @@ func parse_facelist(raw []byte) {
 			// rand.Read(token)
 			// stoken := hex.EncodeToString(token)
 			stoken := "/" + RandStringBytes(16)
-			if strings.Contains(uri, "6363") {
+			if strings.Contains(uri, ":6363") {
 				if _, ok := facelist[faceid]; ok {
 					fmt.Println("Use existing")
 					facelist[faceid] = faces{n_oi: outi, n_in: innack, tkn: facelist[faceid].tkn, ngb: facelist[faceid].ngb, rtt: facelist[faceid].rtt, thg: facelist[faceid].thg}
