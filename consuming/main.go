75488822fc03829ff650bbeb935896c809f2c58e
aderama2711 <ade.ramadha@gmail.com>
2023-06-17 15:05:39 +0700

    new hello procedure

--- consuming/main.go
+++ consuming/main.go
@@ -1,13 +1,241 @@
 package main
 
 import (
+	"context"
 	"encoding/binary"
 	"encoding/hex"
 	"fmt"
+	"log"
 	"math/rand"
 	"strings"
+	"sync"
 	"time"
+
+	"github.com/usnistgov/ndn-dpdk/ndn"
+	"github.com/usnistgov/ndn-dpdk/ndn/endpoint"
+	"github.com/usnistgov/ndn-dpdk/ndn/l3"
+	"github.com/usnistgov/ndn-dpdk/ndn/mgmt"
+	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/nfdmgmt"
 )
+
+func main() {
+	facelist := make(map[uint64]faces)
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	// consumer("/ndn/coba")
+
+	// //Serve /hello interest
+	time.Sleep(1 * time.Second)
+
+	// //hello protocol every 5 second
+	var (
+		client mgmt.Client
+		face   mgmt.Face
+		fwFace l3.FwFace
+	)
+
+	client, e := nfdmgmt.New()
+
+	face, e = client.OpenFace()
+	if e != nil {
+		fmt.Println(e)
+	}
+	l3face := face.Face()
+
+	fw := l3.GetDefaultForwarder()
+	if fwFace, e = fw.AddFace(l3face); e != nil {
+		fmt.Println(e)
+	}
+	fwFace.AddRoute(ndn.Name{})
+	fw.AddReadvertiseDestination(face)
+
+	log.Printf("uplink opened, state is %s", l3face.State())
+	l3face.OnStateChange(func(st l3.TransportState) {
+		log.Printf("uplink state changes to %s", l3face.State())
+	})
+
+	interval := 10 * time.Second
+	interval_interest := 100 * time.Millisecond
+	for {
+		//update facelist
+		update_facelist()
+		fmt.Println(facelist)
+
+		//create route
+		for k, v := range facelist {
+			register_route(v.tkn, 0, int(k))
+
+			fmt.Println(k, v.tkn)
+			//send hello interest to every face
+			interest := ndn.MakeInterest(ndn.ParseName("hello"), ndn.ForwardingHint{ndn.ParseName(v.tkn), ndn.ParseName("hello")})
+
+			data, rtt, thg, e := consumer_interest(interest)
+
+			if e != nil {
+				continue
+			}
+
+			fmt.Println(data)
+
+			v.ngb = data
+			v.rtt = rtt
+			v.thg = thg
+			facelist[k] = v
+
+			time.Sleep(interval_interest)
+		}
+		fmt.Println(facelist)
+
+		time.Sleep(interval)
+	}
+
+	// go producer("hello", "Hello World!", 10)
+
+	// time.Sleep(1 * time.Second)
+
+	// var (
+	// 	client mgmt.Client
+	// 	face   mgmt.Face
+	// 	fwFace l3.FwFace
+	// )
+
+	// client, e := nfdmgmt.New()
+
+	// face, e = client.OpenFace()
+	// if e != nil {
+	// 	fmt.Println(e)
+	// }
+	// l3face := face.Face()
+
+	// fw := l3.GetDefaultForwarder()
+	// if fwFace, e = fw.AddFace(l3face); e != nil {
+	// 	fmt.Println(e)
+	// }
+	// fwFace.AddRoute(ndn.Name{})
+	// fw.AddReadvertiseDestination(face)
+
+	// log.Printf("uplink opened, state is %s", l3face.State())
+	// l3face.OnStateChange(func(st l3.TransportState) {
+	// 	log.Printf("uplink state changes to %s", l3face.State())
+	// })
+
+	// data, _, _, e := consumer("hello")
+	// fmt.Println(data)
+	// fmt.Println(e)
+
+	wg.Wait()
+
+}
+
+func consumer(name string) (content string, rtt float64, thg float64, e error) {
+	// seqNum := rand.Uint64()
+	// var nData, nErrors atomic.Int64
+
+	interest := ndn.ParseName(name)
+
+	t0 := time.Now()
+
+	data, e := endpoint.Consume(context.Background(), ndn.MakeInterest(interest),
+		endpoint.ConsumerOptions{})
+
+	rtt = float64(time.Since(t0).Milliseconds())
+
+	if e == nil {
+		// nDataL, nErrorsL := nData.Add(1), nErrors.Load()
+		// fmt.Println(data.Content)
+		content = string(data.Content[:])
+		// fmt.Printf("%6.2f%% D %s\n", 100*float64(nDataL)/float64(nDataL+nErrorsL), content)
+		thg = float64(len(content)) / float64(rtt/1000)
+	} else {
+		// nDataL, nErrorsL := nData.Load(), nErrors.Add(1)
+		// fmt.Printf("%6.2f%% E %v\n", 100*float64(nDataL)/float64(nDataL+nErrorsL), e)
+		return content, 0, 0, e
+	}
+
+	return content, rtt, thg, nil
+}
+
+func consumer_interest(Interest ndn.Interest) (content string, rtt float64, thg float64, e error) {
+	// seqNum := rand.Uint64()
+	// var nData, nErrors atomic.Int64
+
+	t0 := time.Now()
+
+	data, e := endpoint.Consume(context.Background(), Interest,
+		endpoint.ConsumerOptions{})
+
+	raw_rtt := time.Since(t0)
+
+	rtt = float64(raw_rtt / time.Millisecond)
+
+	fmt.Println(rtt)
+
+	if e == nil {
+		// nDataL, nErrorsL := nData.Add(1), nErrors.Load()
+		// fmt.Println(data.Content)
+		content = string(data.Content[:])
+		// fmt.Printf("%6.2f%% D %s\n", 100*float64(nDataL)/float64(nDataL+nErrorsL), content)
+		thg = float64(len(content)) / float64(rtt/1000)
+	} else {
+		// nDataL, nErrorsL := nData.Load(), nErrors.Add(1)
+		// fmt.Printf("%6.2f%% E %v\n", 100*float64(nDataL)/float64(nDataL+nErrorsL), e)
+		return content, 0, 0, e
+	}
+
+	return content, rtt, thg, nil
+}
+
+func update_facelist() {
+	c, _ := nfdmgmt.New()
+
+	var sigNonce [8]byte
+	rand.Read(sigNonce[:])
+
+	interest := ndn.Interest{
+		Name:        ndn.ParseName("/localhost/nfd/faces/list"),
+		MustBeFresh: true,
+		CanBePrefix: true,
+		SigInfo: &ndn.SigInfo{
+			Nonce: sigNonce[:],
+			Time:  uint64(time.Now().UnixMilli()),
+		},
+	}
+
+	c.Signer.Sign(&interest)
+
+	data, e := endpoint.Consume(context.Background(), interest,
+		endpoint.ConsumerOptions{})
+
+	if e != nil {
+		fmt.Println(e)
+	} else {
+		parse_facelist(data.Content)
+	}
+}
+
+func register_route(name string, cost int, faceid int) {
+
+	c, _ := nfdmgmt.New()
+
+	cr, e := c.Invoke(context.TODO(), nfdmgmt.RibRegisterCommand{
+		Name:   ndn.ParseName(name),
+		Origin: 0,
+		Cost:   cost,
+		FaceID: faceid,
+	})
+
+	if e != nil {
+		fmt.Println(e)
+	}
+	if cr.StatusCode != 200 {
+		fmt.Println("unexpected response status %d", cr.StatusCode)
+	} else {
+		fmt.Println("Route registered")
+	}
+}
 
 type faces struct {
 	ngb  string
--- serving/main.go
+++ serving/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/usnistgov/ndn-dpdk/ndn"
@@ -12,6 +13,54 @@ import (
 	"github.com/usnistgov/ndn-dpdk/ndn/mgmt"
 	"github.com/usnistgov/ndn-dpdk/ndn/mgmt/nfdmgmt"
 )
+
+func main() {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+
+	// consumer("/ndn/coba")
+
+	// //Serve /hello interest
+	producer("hello", "R1", 10)
+
+	// go producer("hello", "Hello World!", 10)
+
+	// time.Sleep(1 * time.Second)
+
+	// var (
+	// 	client mgmt.Client
+	// 	face   mgmt.Face
+	// 	fwFace l3.FwFace
+	// )
+
+	// client, e := nfdmgmt.New()
+
+	// face, e = client.OpenFace()
+	// if e != nil {
+	// 	fmt.Println(e)
+	// }
+	// l3face := face.Face()
+
+	// fw := l3.GetDefaultForwarder()
+	// if fwFace, e = fw.AddFace(l3face); e != nil {
+	// 	fmt.Println(e)
+	// }
+	// fwFace.AddRoute(ndn.Name{})
+	// fw.AddReadvertiseDestination(face)
+
+	// log.Printf("uplink opened, state is %s", l3face.State())
+	// l3face.OnStateChange(func(st l3.TransportState) {
+	// 	log.Printf("uplink state changes to %s", l3face.State())
+	// })
+
+	// data, _, _, e := consumer("hello")
+	// fmt.Println(data)
+	// fmt.Println(e)
+
+	wg.Wait()
+
+}
 
 func producer(name string, content string, fresh int) {
 	payload := []byte(content)
