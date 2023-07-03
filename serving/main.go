c6a830a140804e43e992c89efebb1a0f48acd03a
aderama2711 <ade.ramadha@gmail.com>
2023-07-03 12:02:10 +0700

    code refactor

--- serving/main.go
+++ serving/main.go
@@ -19,44 +19,7 @@ func main() {
 
 	wg.Add(1)
 
-	// consumer("/ndn/coba")
-
-	// //Serve /hello interest
 	producer("hello", "R1", 10)
-
-	// go producer("hello", "Hello World!", 10)
-
-	// time.Sleep(1 * time.Second)
-
-	// var (
-	// 	client mgmt.Client
-	// 	face   mgmt.Face
-	// 	fwFace l3.FwFace
-	// )
-
-	// client, e := nfdmgmt.New()
-
-	// face, e = client.OpenFace()
-	// if e != nil {
-	// 	fmt.Println(e)
-	// }
-	// l3face := face.Face()
-
-	// fw := l3.GetDefaultForwarder()
-	// if fwFace, e = fw.AddFace(l3face); e != nil {
-	// 	fmt.Println(e)
-	// }
-	// fwFace.AddRoute(ndn.Name{})
-	// fw.AddReadvertiseDestination(face)
-
-	// log.Printf("uplink opened, state is %s", l3face.State())
-	// l3face.OnStateChange(func(st l3.TransportState) {
-	// 	log.Printf("uplink state changes to %s", l3face.State())
-	// })
-
-	// data, _, _, e := consumer("hello")
-	// fmt.Println(data)
-	// fmt.Println(e)
 
 	wg.Wait()
 
